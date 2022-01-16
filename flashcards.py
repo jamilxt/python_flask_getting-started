5841956fb64382c76180f4e458e14dee8e44c482
jamilxt <jamilxt@gmail.com>
2022-01-16 22:42:27 +0600

    Intial commit

---
+++ flashcards.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to my Flash Cards application!"
+
+@app.route("/date")
+def date():
+    return "This page was served at " + str(datetime.now())
+
+counter = 0
+
+@app.route("/count_views")
+def count_demo():
+    global counter
+    counter += 1
+    return "This page was served " + str(counter) + " times"
